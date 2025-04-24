67cdfab9dcf26b945579a3b257bc5f6b2e7da4ca
Hobo <fubarferment@gmail.com>
2025-04-23 17:14:08 -0700

    Update speed.lua

--- client/speed.lua
+++ client/speed.lua
@@ -1,8 +1,19 @@
 if Config.Speed.active then
-	Citizen.CreateThread(function()
-		while true do
-			Citizen.Wait(30000)
-			TriggerServerEvent("ac:spdr")
+	local Users = {}
+
+	RegisterServerEvent("ac:spdr")
+	AddEventHandler("ac:spdr", function()
+		local _source = source
+		local t = os.time()
+		if Users[_source] then
+			local t = t
+			if (t - Users[_source].time) < 15 then
+				HandleAntiCheatAction(_source, Config.Speed, Config.Speed.lang.reason)
+			else
+				Users[_source].time = t
+			end
+		else
+			Users[_source] = {time = t}
 		end
 	end)
 end
