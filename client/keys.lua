644506a2dd5d48a26aae18079406247387f99164
Hobo <fubarferment@gmail.com>
2025-04-23 17:46:56 -0700

    Update keys.lua
    Some minor fixes

--- client/keys.lua
+++ client/keys.lua
@@ -12,12 +12,14 @@ if Config.Keys.active then
                 if #keyCombo == 1 then
                     local key1 = keyCombo[1];
                     if IsControlJustReleased(0, key1) then
+                        print("Key violation detected: " .. keyStr) -- Debug log
                         TriggerServerEvent("ac:kick", Config.Keys.lang.kickreason .. ': ' .. keyStr, Config.Keys)
                     end
                 elseif #keyCombo == 2 then
                     local key1 = keyCombo[1];
                     local key2 = keyCombo[2];
                     if IsControlPressed(0, key1) and IsControlPressed(0, key2) then
+                        print("Key combination violation detected: " .. keyStr) -- Debug log
                         TriggerServerEvent("ac:kick", Config.Keys.lang.kickreason .. ': ' .. keyStr, Config.Keys)
                         Wait(20000); -- Wait 20 seconds
                     end
@@ -26,7 +28,8 @@ if Config.Keys.active then
                     local key2 = keyCombo[2];
                     local key3 = keyCombo[3];
                     if IsControlPressed(0, key1) and IsControlPressed(0, key2) and IsControlPressed(0, key3) then
-                            TriggerServerEvent("ac:kick", Config.Keys.lang.kickreason .. ': ' .. keyStr, Config.Keys)
+                        print("Three-key combination violation detected: " .. keyStr) -- Debug log
+                        TriggerServerEvent("ac:kick", Config.Keys.lang.kickreason .. ': ' .. keyStr, Config.Keys)
                     end
                     Wait(20000); -- Wait 20 seconds
                 
@@ -35,6 +38,7 @@ if Config.Keys.active then
                     local key3 = keyCombo[3];
                     local key4 = keyCombo[4];
                     if IsControlPressed(0, key1) and IsControlPressed(0, key2) and IsControlPressed(0, key3) and IsControlPressed(0, key4) then
+                        print("Four-key combination violation detected: " .. keyStr) -- Debug log
                         TriggerServerEvent("ac:kick", Config.Keys.lang.kickreason .. ': ' .. keyStr, Config.Keys)
                     end
                     Wait(20000); -- Wait 20 seconds
@@ -43,4 +47,5 @@ if Config.Keys.active then
         end
     end)
 end
+
 
